9f8069f8793f43224a900eef5341978ecba81895
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-05-27 19:30:46 +0300

    finish basic task level

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import ImagesApiService from './js/fetchImages';
+import { Notify } from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const imageApiService = new ImagesApiService();
+let limitImages = 0;
+let counter = 0;
+let simpleLightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 const refs = {
   searchForm: document.querySelector('.search-form'),
   cardsList: document.querySelector('.gallery'),
-  // buttonMore: document.querySelector('.load-more'),
+  buttonMore: document.querySelector('.load-more'),
 };
-
-const imageApiService = new ImagesApiService();
 
 refs.searchForm.addEventListener('submit', onSearch);
 refs.buttonMore.addEventListener('click', onLoadMore);
@@ -17,7 +26,7 @@ function onSearch(evt) {
   imageApiService.query = evt.currentTarget.elements.searchQuery.value.trim();
 
   if (imageApiService.query === '') {
-    return alert('Введите запрос');
+    return Notify.warning('Please type text.');
   }
 
   refs.cardsList.innerHTML = '';
@@ -25,39 +34,123 @@ function onSearch(evt) {
   imageApiService.resetPage();
   imageApiService
     .fetchImages()
-    .then(hits => {
-      if (hits.length > 0) {
-        markupListImages(hits);
+    .then(response => {
+      if (response.totalHits > 0) {
+        Notify.success(`Hooray! We found ${response.totalHits} images.`);
+        limitImages = response.totalHits;
+        // limitImages = 100;
+        markupListImages(response.hits);
+
+        refs.buttonMore.classList.remove('is-hidden');
       } else {
-        console.log('ничего не найдено.');
+        Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
       }
     })
-    .catch(error => console.log(error));
+    .catch(error => console.log(error))
+    .finally(() => {
+      simpleLightbox.refresh();
+    });
 }
 
 function onLoadMore() {
+  refs.buttonMore.classList.add('is-hidden');
   imageApiService
     .fetchImages()
-    .then(hits => markupListImages(hits))
-    .catch(error => console.log(error));
+    .then(response => {
+      markupListImages(response.hits);
+      refs.buttonMore.classList.remove('is-hidden');
+    })
+    .catch(error => console.log(error))
+    .finally(() => {
+      simpleLightbox.refresh();
+    });
 }
 
 function markupListImages(hits) {
-  const markup = hits.map(hit => {
-    return `
+  for (let i = 0; i < hits.length; i++) {
+    const hit = hits[i];
+    counter++;
+    // console.log(counter);
+    // console.log(limitImages);
+
+    if (counter > limitImages) {
+      Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+      break;
+    }
+
+    const markup = `
       <div class="photo-card">
-      <a class="gallery-link" href="${hit.largeImageURL}">
-        <img class="gallery-image" src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" width="300" />
-        <div class="info">
+        <a class="gallery-link" href="${hit.largeImageURL}">
+          <img class="gallery-image" src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" width="300" />
+          <div class="info">
             <p class="info-item"><b>Likes</b>${hit.likes}</p>
             <p class="info-item"><b>Views</b>${hit.views}</p>
             <p class="info-item"><b>Comments</b>${hit.comments}</p>
             <p class="info-item"><b>Downloads</b>${hit.downloads}</p>
-        </div>
+          </div>
         </a>
-      </div>
-      `;
-  });
-  // refs.cardsList.innerHTML = markup.join('');
-  refs.cardsList.insertAdjacentHTML('beforeend', markup.join(''));
+      </div>`;
+
+    refs.cardsList.insertAdjacentHTML('beforeend', markup);
+  }
 }
+
+// function markupListImages(hits) {
+//   const markup = hits.map(hit => {
+//     counter++;
+//     console.log(counter);
+//     console.log(limitImages);
+//     if (counter >= limitImages) {
+//       Notify.failure(
+//         "We're sorry, but you've reached the end of search results."
+//       );
+//       return '';
+//     } else {
+//       return `
+//       <div class="photo-card">
+//       <a class="gallery-link" href="${hit.largeImageURL}">
+//         <img class="gallery-image" src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" width="300" />
+//         <div class="info">
+//             <p class="info-item"><b>Likes</b>${hit.likes}</p>
+//             <p class="info-item"><b>Views</b>${hit.views}</p>
+//             <p class="info-item"><b>Comments</b>${hit.comments}</p>
+//             <p class="info-item"><b>Downloads</b>${hit.downloads}</p>
+//         </div>
+//         </a>
+//       </div>
+//       `;
+//     }
+//   });
+//   // refs.cardsList.innerHTML = markup.join('');
+//   refs.cardsList.insertAdjacentHTML('beforeend', markup.join(''));
+// }
+
+// function markupListImages(hits) {
+//   const limitImages = response.data.totalHits;
+//   const markup = hits.reduce((acc, hit) => {
+//     return (
+//       acc +
+//       `<div class="photo-card">
+//       <a class="gallery-link" href="${hit.largeImageURL}">
+//         <img class="gallery-image"
+//                 src="${hit.webformatURL}"
+//                 alt="${hit.tags}"
+//                 loading="lazy"
+//                 width="300" />
+//         <div class="info">
+//             <p class="info-item"><b>Likes</b>${hit.likes}</p>
+//             <p class="info-item"><b>Views</b>${hit.views}</p>
+//             <p class="info-item"><b>Comments</b>${hit.comments}</p>
+//             <p class="info-item"><b>Downloads</b>${hit.downloads}</p>
+//         </div>
+//         </a>
+//       </div>`
+//     );
+//   }, '');
+
+//   refs.cardsList.insertAdjacentHTML('beforeend', markup);
+// }
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,28 +1,54 @@
+import axios from 'axios';
+
 export default class ImagesApiService {
   constructor() {
     this.searchQuery = '';
     this.page = 1;
   }
 
-  fetchImages() {
-    const url = 'https://pixabay.com/api/';
+  async fetchImages() {
+    const baseUrl = 'https://pixabay.com/api/';
 
-    return fetch(
-      `${url}?key=36679994-fdec862cf8a926d17bd3d37a5&q=${this.searchQuery}
-      &image_type=photo&orientation=horizontal&safesearch=true
-      &page=${this.page}&per_page=40&`
-    )
+    return await axios
+      .get(`${baseUrl}`, {
+        params: {
+          key: `36686955-32d5f33599a736e8573496700`,
+          q: this.searchQuery,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: 'true',
+          per_page: 40,
+          page: this.page,
+        },
+      })
       .then(response => {
-        if (!response.ok) {
+        if (response.status !== 200) {
           throw new Error(response.status);
         }
-        return response.json();
-      })
-      .then(({ hits }) => {
         this.incrementPage();
-        return hits;
+        return response.data;
       });
   }
+
+  // fetchImages() {
+  //   const url = 'https://pixabay.com/api/';
+
+  //   return fetch(
+  //     `${url}?key=36679994-fdec862cf8a926d17bd3d37a5&q=${this.searchQuery}
+  //     &image_type=photo&orientation=horizontal&safesearch=true
+  //     &page=${this.page}&per_page=40&`
+  //   )
+  //     .then(response => {
+  //       if (!response.ok) {
+  //         throw new Error(response.status);
+  //       }
+  //       return response.json();
+  //     })
+  //     .then(hits => {
+  //       this.incrementPage();
+  //       return hits;
+  //     });
+  // }
 
   incrementPage() {
     this.page += 1;
